2d58fc5f2c17c549ee1043938cdc27f6d1488e82
GilderlanSC <151675698+GilderlanSC@users.noreply.github.com>
2023-12-14 16:20:52 -0300

    Update main.js

--- Wearable/PowerAPI/js/main.js
+++ Wearable/PowerAPI/js/main.js
@@ -1,17 +1,4 @@
-//function myFunction() {
-//	var screenBrightness = tizen.power.getScreenBrightness();
-//	const input = document.querySelector("vol");
-	//const value = document.querySelector("value");
-	
-//	value.textContent = input.value;
-	//console.log(value)
-	//i/nput.addEventListener("input", function(e){
-	//	  value.textContent = event.target.value;
-	//	});
 
-	//tizen.power.setScreenBrightness(0.5);
-	//
-//}
 function myFunction(){
 const value = document.querySelector("#value");
 const input = document.querySelector("#pi_input");
